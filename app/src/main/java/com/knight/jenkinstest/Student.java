18e4062e55648afe381fd9e6f5e964c72cbfcaf9
KnightOneAdmin <37442216knight@gmail.com>
2018-05-03 14:29:48 +0800

    *fix 添加测试maven

---
+++ app/src/main/java/com/knight/jenkinstest/Student.java
@@ -0,0 +1,12 @@
+package com.knight.jenkinstest;
+
+/**
+ * description: ${TODO}
+ * autour: Knight
+ * new date: 2018/5/3 on 14:29
+ * e-mail: 37442216knight@gmail.com
+ * update: 2018/5/3 on 14:29
+ * version: v 1.0
+ */
+public class Student {
+}
